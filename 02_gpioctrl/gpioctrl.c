51a9c755f366b64996c6dc194c889eb0a9b5675f
Hardik01363 <hardikkhandelwal444@gmail.com>
2026-06-24 17:08:11 +0530

    some progress in 02

--- 02_gpioctrl/gpioctrl.c
+++ 02_gpioctrl/gpioctrl.c
@@ -1,8 +1,26 @@
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/gpio/consumer.h>
+
+#define IO_LED 21
+#define IO_BUTTON 20
+#define IO_OFFSET 0 //May be different for another Raspberry pi (0 for mine)
+
+static struct gpio_desc *led, *button;
 
 static int __init my_init(void) {
-    printk("hello - Konnichiwa, Kernel-san!\n");
+    led = gpio_to_desc(IO_LED + IO_OFFSET);
+    if(!led){
+        printk("gpioctrl - Error getting pin 21");
+        return -ENODEV;
+    }
+
+    button = gpio_to_desc(IO_BUTTON + IO_OFFSET);
+    if(!button){
+        printk("gpioctrl - Error getting pin 20");
+        return -ENODEV;
+    }
+
     return 0;
 }
 
