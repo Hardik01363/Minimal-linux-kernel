f918c77ec04bb2ee60b1ff8c0f584280dd4d3997
Hardik01363 <hardikkhandelwal444@gmail.com>
2026-07-11 18:48:09 +0530

    Implemented open() function (pointer remaining)

--- 03_char_dev/hello_char_dev.c
+++ 03_char_dev/hello_char_dev.c
@@ -9,6 +9,11 @@ static ssize_t my_read(struct file *f, char __user *u, size_t s, loff_t *l) {
     return 0;
 }
 
+static int my_open(struct inode *inode_ptr, struct file *file_ptr) {
+    ///the struct file, accessed by file_ptr exists only until the file is open.
+    pr_info("hello_char_dev - Major Device Number: %d, Minor Device Number: %d\n", imajor(inode_ptr), iminor(inode_ptr));
+}
+
 static struct file_operations fops = {
     .read = my_read
 };
@@ -16,8 +21,11 @@ static struct file_operations fops = {
 static int __init my_init(void) {
     //arg 0 allocates a free major dev num itself with all of its 256 minor device numbers. if a specific number passed, all 256 minor dev nums of it are allocated to the user. But, then, 0 is returned on successful registration and error code on failure. so, this code snippet would require some minor changes to fetch the major device number.
     major_dev_num = register_chrdev(0, "hello_char_dev", &fops);
-    if(major_dev_num < 0) printk("hello_char_dev - Error registering chrdev\n");
-    printk("hello_char_dev - chrdev registered with Major Devive Number: %d\n", major_dev_num);
+    if(major_dev_num < 0) {
+        pr_err("hello_char_dev - Error registering chrdev\n");
+        return major_dev_num;
+    }
+    pr_info("hello_char_dev - chrdev registered with Major Devive Number: %d\n", major_dev_num);
     return 0;
 }
 
